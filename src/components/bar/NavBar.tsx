8c09fcc9fc182de11fa9db65030ab5b00a714a06
신 우현 <woo_hyun_1@naver.com>
2023-09-19 03:22:34 +0900

    [feat] Create command line on navbar

--- src/components/bar/NavBar.tsx
+++ src/components/bar/NavBar.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const NavBar = () => {
-  return <NavBarContainer></NavBarContainer>;
+  const [command, setCommand] = useState("");
+  return (
+    <NavBarContainer>
+      <CommandLine
+        type="text"
+        placeholder="Command Line"
+        value={command}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setCommand(e.target.value)
+        }
+      />
+    </NavBarContainer>
+  );
 };
 
 export default NavBar;
@@ -10,12 +22,24 @@ export default NavBar;
 const NavBarContainer = styled.header`
   width: 100%;
   height: 66px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   position: fixed;
   top: 0;
   left: 304;
   background-color: white;
+  padding: 12px 32px;
   /* top: 0;
   position: fixed;
   left: 256px; */
   box-shadow: 0 3px 3px rgba(0, 0, 0, 0.2);
+`;
+
+const CommandLine = styled.input`
+  width: 1000px;
+  padding: 12px 20px;
+  border: 1px solid #919eab;
+  border-radius: 6px;
+  color: #919eab;
 `;
